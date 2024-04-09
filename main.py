aad7fadaf96a77f8f8652e5370927555e3323ba5
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-04-09 15:31:54 -0400

    Add lines in equal intervals

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False,margin=0)
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
+
+
+def line_work():
+    for index_local in range(25, 285, 10):
+        pdf.line(10, index_local, 200, index_local)
 
 
 for index, row in df.iterrows():
@@ -11,24 +16,23 @@ for index, row in df.iterrows():
 
     # Set the header
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 25, 200, 25)
 
     # Set the footer
     pdf.ln(260)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+    line_work()
 
-    for i in range(row["Pages"]-1):
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-
         # Set the footer
         pdf.ln(275)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
-
+        line_work()
 
 pdf.output("output.pdf")
